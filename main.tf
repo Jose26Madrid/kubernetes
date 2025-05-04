b72d88fb5e926b7e316433dc125744c678c11c6c
JOSE IGNACIO MAGARIÑO-RAMIRO <42111553+Jose26Madrid@users.noreply.github.com>
2025-05-04 03:16:27 +0200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,74 @@
+```hcl
+
+# main.tf - Terraform EKS Cluster con Instancias SPOT
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.1.1"
+
+  name = "eks-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1b"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.8.4"
+
+  cluster_name    = "eks-spot-cluster"
+  cluster_version = "1.29"
+
+  subnet_ids = module.vpc.private_subnets
+  vpc_id     = module.vpc.vpc_id
+
+  enable_irsa = true
+
+  eks_managed_node_groups = {
+    spot = {
+      desired_size = 2
+      max_size     = 3
+      min_size     = 1
+
+      instance_types = ["t3.medium", "t3.small"]
+      capacity_type  = "SPOT"
+
+      tags = {
+        Name = "eks-spot-node"
+      }
+    }
+  }
+
+  tags = {
+    Environment = "dev"
+    Terraform   = "true"
+  }
+}
+
+output "cluster_name" {
+  value = module.eks.cluster_name
+}
+
+output "kubeconfig" {
+  value = module.eks.kubeconfig
+}
+
+output "cluster_endpoint" {
+  value = module.eks.cluster_endpoint
+}
+
+```
